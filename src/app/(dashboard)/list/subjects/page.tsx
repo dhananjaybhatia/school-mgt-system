ee810dc65f3c6219da9f4beeac856f77f3bf3aaa
dhananjaybhatia <dhananjaybhatia@yahoo.co.in>
2025-07-04 17:31:02 +1000

    subject page - api connected, pagination and search input is also done

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -2,14 +2,18 @@ import FormModal from "@/src/components/FormModal";
 import Pagination from "@/src/components/Pagination";
 import Table from "@/src/components/Table";
 import TableSearch from "@/src/components/TableSearch";
-import { role, subjectsData } from "@/src/lib/data";
+import { Lesson, Teacher } from "@/src/generated/prisma";
+import { role } from "@/src/lib/data";
+import { prisma } from "@/src/lib/prisma";
+import { ITEM_PER_PAGE } from "@/src/lib/utils";
 import Image from "next/image";
 
-type Subjects = {
-  id: number;
-  name: string;
-  teachers: string[];
-};
+// type Subjects = {
+//   id: number;
+//   name: string;
+//   teachers: string[];
+// };
+type SubjectList = Subject & { teachers: Teacher[] };
 
 const columns = [
   {
@@ -28,30 +32,73 @@ const columns = [
   },
 ];
 
-const SubjectsListPage = () => {
-  const renderRow = (item: Subjects) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-[#f2f1ff]"
-    >
-      <td className="flex items-center gap-4 p-4">
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.teachers.join(", ")}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              <FormModal table={"subject"} type={"update"} data={item} />
-              <FormModal table={"subject"} type={"delete"} id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: SubjectList) => (
+  <tr
+    key={item.id}
+    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-[#f2f1ff]"
+  >
+    <td className="flex items-center gap-4 p-4">
+      <div className="flex flex-col">
+        <h3 className="font-semibold">{item.name}</h3>
+      </div>
+    </td>
+    <td className="hidden md:table-cell">
+      {item.teachers.map((teacher: Teacher) => teacher.name).join(", ")}
+    </td>
+    <td>
+      <div className="flex items-center gap-2">
+        {role === "admin" && (
+          <>
+            <FormModal table={"subject"} type={"update"} data={item} />
+            <FormModal table={"subject"} type={"delete"} id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+const SubjectsListPage = async ({
+  searchParams = {},
+}: {
+  searchParams?: { [key: string]: string };
+}) => {
+  const { page, ...queryParams } = searchParams;
+  const p = page ? parseInt(page) : 1;
+
+  // URL PARAMS RULES
+
+  const query: Prisma.SubjectWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "search":
+            query.name = {
+              contains: value,
+              mode: "insensitive", // case-insensitive search
+            };
+            break;
+
+          // You can add more filters here in future
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.subject.findMany({
+      where: query,
+      include: {
+        teachers: true,
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+
+    prisma.subject.count({ where: query }),
+  ]);
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
@@ -74,8 +121,8 @@ const SubjectsListPage = () => {
       </div>
 
       <div>
-        <Table columns={columns} renderRow={renderRow} data={subjectsData} />
-        <Pagination />
+        <Table columns={columns} renderRow={renderRow} data={data} />
+        <Pagination page={p} count={count} />
       </div>
     </div>
   );
